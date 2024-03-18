ec316c0a122af70fc89b859fbc86e708bf2c1a83
MaulanaDevtech422 <163699226+MaulanaDevtech422@users.noreply.github.com>
2024-03-18 09:17:57 +0700

    Create prompt.sh
    Prompt for termux

---
+++ prompt.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+# Clear the terminal screen
+clear
+red='\033[0;34m'
+green='\033[0;32m'
+r="\033[0;35m"
+y="\033[0;34m"
+t="\033[0;33m"
+# Print the title using figlet
+echo ""
+figlet -f slant "MaulanaDevtech" | lolcat
+./b.sh | lolcat
+# Print the bio description
+echo -e "${y}++++++++++++++++++++++++++++++++++++"
+echo -e "${y}+            ${t}contact us           ${y} +"
+echo -e "${y}++++++++++++++++++++++++++++++++++++"
+echo -e "${y}+ ${t}Gmail     :technocode@gmail.com  ${y}+"
+echo -e "+ ${t}No Tel    :+6285712816240      ${y}  +"
+echo -e "+ ${t}Instagram :ikr.maulana422   ${y}     +"
+echo -e "+ ${t}Telegram  :@technocode422     ${y}   +"
+echo -e "${y}++++++++++++++++++++++++++++++++++++"
+echo ""
+echo -e "${red}========================================================================================"
+echo -e "${red}+                                  ${r}ABOUT MAULANADEVTECH                                ${red}+"
+echo -e "${red}========================================================================================"
+echo -e "${red}+ [÷]Web Developer and Back End Developer specializing in Python                       +"
+echo "+ with a passion for creating robust and scalable web applications.                    +"
+echo "+ [÷]Experienced in Django, FastAPI, Flask, HTML, CSS, and JavaScript                  +"
+echo "+ [÷]React, Node.js, and Express.js. I am also familiar with                           +"
+echo "+ database management systems like MongoDB and PostgreSQL.                             +"
+echo "+ [÷]Always eager to learn and adapt to new technologies and trends in web development.+"
+echo "+ [÷]I have a good knowledge of the basic concepts of Python                           +"
+echo "+ such as OOP (object oriented programming), list comprehension, and generators.       +"
+echo "+ [÷]I am also familiar with several tools and technologies commonly used in the       +"
+echo "+ web development world, such as HTML, CSS, JavaScript, Git, and Docker.               +"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
